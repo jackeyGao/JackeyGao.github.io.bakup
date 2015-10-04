890756c33ee7e6140fb32dca1af270f6cf431053
jackeyGao <gaojunqi@outlook.com>
2015-10-04 18:53:19 +0800

    Site updated: 2015-10-04 18:53:18

--- js/fancy-box.js
+++ js/fancy-box.js
@@ -1,19 +1,24 @@
 $(document).ready(function() {
+  $('.content img').not('.group-picture img').each(function () {
 
-  $('.content img').each(function () {
     var $image = $(this);
     var $imageWrapLink = $image.parent('a');
 
     if ($imageWrapLink.size() < 1) {
       $imageWrapLink = $image.wrap('<a href="' + this.getAttribute('src') + '"></a>').parent('a');
     }
+
     $imageWrapLink.addClass('fancybox');
+
     if ($image.attr("alt")) {
       $imageWrapLink.append('<div class="pic-title"><span>' + $image.attr("alt") + '</span></div>');
-      $imageWrapLink.attr("title",$image.attr("alt")); //make sure img title tag will show correctly in fancybox
-    };
+
+      //make sure img title tag will show correctly in fancybox
+      $imageWrapLink.attr("title", $image.attr("alt"));
+    }
   });
 });
+
 $('.fancybox').fancybox({
   helpers: {
     overlay: {
